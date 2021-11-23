fa5b26dd60c61193e8a733c693061788d323052a
ankitoscar <ankitoscar911@gmail.com>
2021-11-23 15:55:56 +0530

    Added url and view for dashboard

--- app/home/views.py
+++ app/home/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home/homepage.html')
+
+def ticket_data(request):
+    if request.method == 'POST':
+        _to = request.POST['']
+
+def dashboard(request):
+    return render(request, 'home/dashboard.html')
