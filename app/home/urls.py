a792a3d6b4377732d526d6c962291fa0aa9de097
ankitoscar <ankitoscar911@gmail.com>
2021-11-24 15:16:50 +0530

    Added more dashboard views giving JSON responses

--- app/home/urls.py
+++ app/home/urls.py
@@ -5,5 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('dashboard/', views.dashboard, name='dashboard'),
-    path('ticket_data/', views.ticket_data, name='ticket-data'),
+    path('dashboard/busiest_month', views.busiest_month, name='busiest_month'),
+    path('dashboard/price_analysis', views.price_analysis, name='price_analysis'),
+    path('dashboard/on_time', views.on_time_performance, name='on_time'),
+    path('dashboard/most_travelled', views.most_travelled, name="most_travelled"),
+    path('dashboard/most_booked', views.most_booked, name='most_booked'),
 ]
--- app/home/views.py
+++ app/home/views.py
@@ -2,15 +2,31 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .dashboard import Dashboard
 
+d = Dashboard()
+
 # Create your views here.
 def home(request):
     return render(request, 'home/homepage.html')
 
-def ticket_data(request):
-    d = Dashboard()
+def busiest_month(request):
     data = d.busiest_month('BOM', '2017', 'ARRIVING')
     return JsonResponse(data, safe=False)
 
+def price_analysis(request):
+    data = d.price_analysis('BOM', 'DEL', '2021-11-23')
+    return JsonResponse(data, safe=False)
+
+def on_time_performance(request):
+    data = d.on_time_performance('BOM', '2021-11-23')
+    return JsonResponse(data, safe=False)
+
+def most_travelled(request):
+    data = d.most_travelled('BOM', '2017-12')
+    return JsonResponse(data, safe=False)
+
+def most_booked(request):
+    data = d.most_booked('BOM', '2017-12')
+    return JsonResponse(data, safe=False)
 
 def dashboard(request):
     return render(request, 'home/dashboard.html')
